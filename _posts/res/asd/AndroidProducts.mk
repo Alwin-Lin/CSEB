2fb84a55b7179f99d2ef9acc87bd24b25a72965f
Alwin_Lin <alwin001@gmail.com>
2022-01-09 10:46:06 -0800

    remove useless post

--- _posts/res/asd/AndroidProducts.mk
+++
@@ -1,6 +0,0 @@
-# All ASD product build targets
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aphone/aphone.mk
-
-COMMON_LUNCH_CHOICES := \
-    aphone-userdebug
--- _posts/res/asd/aphone/aphone_product.mk
+++
@@ -1,4 +0,0 @@
-# This is aphone AVD product partition build configuration.
-
-# Inherit AOSP product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
